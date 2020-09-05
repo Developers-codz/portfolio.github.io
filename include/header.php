f2d10ca6bb6b73f403601b8dc6cd86d79585ccf7
Kajal kumari <50793296+Developers-codz@users.noreply.github.com>
2020-09-05 17:59:35 +0530

    Portfolio

---
+++ include/header.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg    navbar-default navbar-dark   ">
+  <a class="navbar-brand " href="#"  >PORFOLIO</a>
+  <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- <a href="#" class=" fa fa-bars"></a> -->
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link  mx-3"   href="#profile" onclick="toProfile()">Profile <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  mx-3"   href="#project"  onclick="toProject()">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  mx-3"  href="#journal"  onclick="toJournal()">Journal</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link  mx-3"  href="#contact"  onclick="toContact()">Contact</a>
+      </li>
+      
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    
+    </form>
+  </div>
+</nav>
+
---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Kajal Kumari">
+    <meta name="author" content="https://www.linkedin.com/in/kajal-kumari-52bab41aa/">
+    <title>Portfolio || Kajal Kumari</title>
+
+    <link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css'>
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+ 
+
+ <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="cssFiles/index.css">
+</head>
+<body >
+    <?php
+    include "include/header.php"
+    ?>
+    <div class="container-fluid px-0">
+        <img src="imgFiles/bgPortfolio.jpg" alt="" class="folioImg">
+        <div class="name">Kajal kumari
+        <h2 class="h2 my-3">Currently BCA Pursuing</h2>
+       
+        </div>
+        <!-- <h1 class="ml6">
+  <span class="text-wrapper">
+    <span class="letters">Beautiful Questions</span>
+  </span>
+</h1> -->
+
+        <div class="container">
+            <h1 class="display-4 text-center profile my-3">Profile</h1>
+            <hr class="w-25 mx-auto" style="border-top:1px solid black"/>
+            <div class="row">
+                <div class="col-sm-4">
+        <h3 class="display-5 skills my-4 ">Skills </h3>
+        <ul class=" " type="circle ">
+            <li class="links" >C</li>
+            <li class="links">C++</li>
+            <li class="links">Python</li>
+            <li class="links">Html</li>
+            <li class="links">CSS3</li>
+            <li class="links">Javascript</li>
+            <li class="links">Bootstrap</li>
+            <li class="links">Jquery</li>
+        </ul>
+                </div>
+            <div class="col-sm-4">
+                <h3 class="display-5 skills my-4 ">Industrial Knowledge</h3>
+                <ul class=" " type="circle ">
+                    <li class="links" >Responsive Web Design</li>
+                    <li class="links">Web Development</li>
+                </ul>
+                </div>
+                <div class="col-sm-4 gitThirdCol">
+
+                    <h3 class="display-5 skills my-4 mx-5 pl-5">Github Link</h3>
+                    <a href="https://github.com/Developers-codz" class="mx-5 gitlink">View Here</a>
+                </div>
+            </div>
+       
+    </div>
+    </div>
+    <div class="container">
+        <h2 class="heading text-center display-4">Projects</h2>
+        <hr class="w-25 mx-auto" style="border-top:1px solid black"/>
+        <div class="row justify-content-center">
+            <div class="col-sm-3 my-3 py-4 mx-4 text-center">
+                <h3 class="project">Number Convertor</h3>
+                <a href="numberconvertor.html" class="text-decoration-none btn my-5">View Project</a>
+                <a href="#" class="text-decoration-none btn my-5">Go to Code</a>
+            </div>
+            <div class="col-sm-3 my-3 py-4 mx-4 text-center ">
+            <h3 class="project">Color Picker</h3>
+                <a href="colorpicker.html" class="text-decoration-none btn my-5">View Project</a>
+                <a href="#" class="text-decoration-none btn my-5">Go to Code</a>
+            </div>
+            <div class="col-sm-3 my-3 py-4 mx-4 text-center">
+            <h3 class="project">Tic Tac Toe Game</h3>
+                <a href="tic-tac-toe.html" class="text-decoration-none btn my-5">View Project</a>
+                <a href="#" class="text-decoration-none btn my-5">Go to Code</a>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+        <div class="col-sm-3 my-3 py-4 mx-4 text-center ">
+            <h3 class="project">Designed Website</h3>
+                <a href="#" class="text-decoration-none btn my-5">View Project</a>
+                <a href="#" class="text-decoration-none btn my-5">Go to Code</a>
+            </div>
+        <div class="col-sm-3 my-3 py-4 mx-4 text-center ">
+            <h3 class="project">Python ChatBot</h3>
+                <a href="#" class="text-decoration-none btn my-5">View Project</a>
+                <a href="#" class="text-decoration-none btn my-5">Go to Code</a>
+            </div>
+        </div>
+    </div>
+    <!-- footer starts-->
+            <div class="footer bg-dark text-light mt-5 " id="foot">
+              <!-- Copyright -->
+              
+      <div class="footer-copyright text-center py-3 mx-3">© 2020 Copyright:
+        <a href="#" class="text-decoration-none text-light">developers-codz  || Kajal Kumari</a>
+      </div>
+      <i  id="myBtn" onclick="topFunction()" class="fa fa-angle-double-up" style="font-size:20px"></i>
+      <!-- Copyright -->
+     
+            </div>
+             <!-- footer Ends -->
+    </div>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="jsFiles/index.js"></script>
+</body>
+</html>
